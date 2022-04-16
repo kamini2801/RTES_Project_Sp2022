7b113b91869bb55c768dc0d5c2067bce669306f4
kamini2801 <kamini.budke@colorado.edu>
2022-04-16 14:53:34 -0600

    feat: printing demos with load

--- trig.c
+++ trig.c
@@ -14,7 +14,7 @@ int a, b, c;
 
 void main(){
     struct timeval timeNow;
-    int d=10, x=10, y=10, a=8;
+    int d=8, x=1, y=6, a1=10;
     printf("hello\n");
 
     printf("Enter distance D from wall: \n");
@@ -26,32 +26,38 @@ void main(){
     printf("Enter y coordinates: \n");
     //scanf("%d", y);
 
-    printf("x y z: %d %d %d\n", x, y, d);
+    printf("a y d: %d %d %d\n", a, y, d);
 
     gettimeofday(&timeNow, (void *)0);
     long start = timeNow.tv_usec;
     //a
     a = (4*d*d + 4*y*y);
+    printf("Calculated a: %d \n", a);
 
-    b = -2*d*(d*d + y*y);
+    b = -2*d*(d*d + y*y);   
+    printf("Calculated b: %d \n", b);
 
-    c = (d*d + y*y)*(d*d + y*y) - 4*(y*y*a*a);
+    c = (((d*d) + (y*y))*((d*d) + (y*y))) - (4*(y*y)*(a1*a1));
+    printf("Calculated c: %d \n", c);
 
-    double ans1 = (-b + sqrt(b*b - 4*a*c))/2*a;     //x1
+    float check = b*b - 4*a*c;
+    printf("Calculated check: %f \n", check);
 
-    double ans2 = (-b - sqrt(b*b - 4*a*c))/2*a;     //x1
+    float ans1 = (-b + sqrt(b*b - 4*a*c))/2*a;     //x1
 
-    printf("Calculated x1s: %ld %ld ", ans1, ans2);
+    float ans2 = (-b - sqrt(b*b - 4*a*c))/2*a;     //x1
 
-    float angle1 = atan( (sqrt(a*a - ans1*ans1))/ans1 );
+    printf("Calculated x1s: %f %f \n", ans1, ans2);
 
-    float angle2 = atan( (sqrt(a*a - ans1*ans1))/ans1 );
+    float angle1 = atan( (sqrt(a1*a1 - ans1*ans1))/ans1 );
 
-    printf("Calculated angles: %f %f ", angle1, angle2);
+    float angle2 = atan( (sqrt(a1*a1 - ans1*ans1))/ans1 );
+
+    printf("Calculated angles: %f %f \n", angle1, angle2);
 
     gettimeofday(&timeNow, (void *)0);
 
-    printf("Time diff: %ld ", timeNow.tv_usec - start);
+    printf("Time diff: %ld \n", timeNow.tv_usec - start);
 
 
 }
